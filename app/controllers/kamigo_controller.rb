904046ecf4277e4dce1a71a1060fee80af51b68d
siryus <siryus1532@gmail.com>
2021-08-24 22:40:39 +0800

    line bot4

--- app/controllers/kamigo_controller.rb
+++ app/controllers/kamigo_controller.rb
@@ -3,11 +3,12 @@ class KamigoController < ApplicationController
   protect_from_forgery with: :null_session
 
   def webhook
+
     # 學說話
-    reply_text = learn(channel_id, received_text)
+    reply_text = learn(received_text)
 
     # 關鍵字回覆
-    reply_text = keyword_reply(channel_id, received_text) if reply_text.nil?
+    reply_text = keyword_reply(received_text) if reply_text.nil?
 
     # 推齊
     reply_text = echo2(channel_id, received_text) if reply_text.nil?
@@ -18,18 +19,18 @@ class KamigoController < ApplicationController
 
     # 傳送訊息到 line
     response = reply_to_line(reply_text)
-
+    
     # 回應 200
     head :ok
   end 
 
-  # 頻道 ID
+    # 頻道 ID
   def channel_id
     source = params['events'][0]['source']
     source['groupId'] || source['roomId'] || source['userId']
   end
 
-  # 儲存對話
+    # 儲存對話
   def save_to_received(channel_id, received_text)
     return if received_text.nil?
     Received.create(channel_id: channel_id, text: received_text)
@@ -52,6 +53,7 @@ class KamigoController < ApplicationController
 
     received_text
   end
+
 
   # 取得對方說的話
   def received_text
@@ -76,11 +78,9 @@ class KamigoController < ApplicationController
     KeywordMapping.create(keyword: keyword, message: message)
     '好哦～好哦～'
   end
-  
+
   # 關鍵字回覆
-  def keyword_reply(channel_id, received_text)
-    message = KeywordMapping.where(channel_id: channel_id, keyword: received_text).last&.message
-    return message unless message.nil?
+  def keyword_reply(received_text)
     KeywordMapping.where(keyword: received_text).last&.message
   end
 
@@ -101,13 +101,14 @@ class KamigoController < ApplicationController
     line.reply_message(reply_token, message)
   end
 
- # Line Bot API 物件初始化
+  # Line Bot API 物件初始化
   def line
     @line ||= Line::Bot::Client.new { |config|
        config.channel_secret = '068642867953c0cde3987cb696dccac7'
     config.channel_token = 'VUzbj9NMqRMCyDkbT3STQaXDCpIL7cMhLCTMbkfi153QP3RYghdWgcdFnWs02OHj5UvCZAuW/wsnBgLRwcC/o7dA1Pize8UG8A5Dsr/kIiw1t88GCVFBv8zAQW9jPiqtMIxArSfoXsctpvEN13SpwgdB04t89/1O/w1cDnyilFU='
     }
   end
+
 
   def eat
     render plain: "吃土啦"
@@ -155,5 +156,4 @@ class KamigoController < ApplicationController
   def translate_to_korean(message)
     "#{message}油~"
   end
-
 end
