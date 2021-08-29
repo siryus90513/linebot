2dab468f294795a6b317ec93f932fbcc91995b9d
siryus <siryus1532@gmail.com>
2021-08-30 01:35:06 +0800

    miller050

--- app/controllers/kamigo_controller.rb
+++ app/controllers/kamigo_controller.rb
@@ -6,36 +6,34 @@ class KamigoController < ApplicationController
       # 查天氣
   reply_image = get_weather(received_text) 
 
-    # 有查到的話 後面的事情就不作了
-    unless reply_image.nil?
-      # 傳送訊息到 line
-      response = reply_image_to_line(reply_image)
+  # 有查到的話 後面的事情就不作了
+  unless reply_image.nil?
+    # 傳送訊息到 line
+    response = reply_image_to_line(reply_image)
 
-      # 回應 200
-      head :ok
+    # 回應 200
+    head :ok
 
-      return 
-    end
+    return 
+  end
 
 
     # 學說話
-    reply_text = learn(channel_id, received_text)  
-
-    # 隨機數字
-    reply_text = randnum(received_text) 
-
-    # choosedinner
-    reply_text = choosedinner(received_text) if reply_text.nil?
+    reply_text = learn(channel_id, received_text)
+    
+     # random
+    reply_text = feeling(received_text) if reply_text.nil?
 
     # chooselunch 
     reply_text = chooselunch(received_text) if reply_text.nil?
 
-    # 推齊
+     # 推齊
     reply_text = echo2(channel_id, received_text) if reply_text.nil?
 
     # 關鍵字回覆
     reply_text = keyword_reply(channel_id, received_text) if reply_text.nil?
 
+   
 
    
 
@@ -53,7 +51,7 @@ class KamigoController < ApplicationController
 
   def get_weather(received_text)
     return nil unless received_text.include? '天氣'
-    get_weather_from_cwb
+    upload_to_imgur(get_weather_from_cwb)
   end
 
   #增加一個上傳圖片到 imgur 的函數
@@ -141,41 +139,20 @@ class KamigoController < ApplicationController
 
   end
 
-  # 從輸入選項中選一個
+  def feeling(received_text)
 
-  # 隨機數字 69
-  def randnum (received_text)
-
-    return nil unless received_text[0..3] == '隨機數字'
-    
-    received_text = received_text[5..-1]
+    return nil unless received_text[0..6] == '米煮波心情如何'
 
 
-
-    message = rand(received_text.to_i).to_s
-    message
-  end
-
-
-
-  def choosedinner(received_text)
-
-    return nil unless received_text[0..3] == '晚餐吃啥'
       
-     ['武媽媽','81HOME','豪緯麵食館','鍋道一號','外賣','咖哩拌飯','7-11','吃我','不要吃','八方雲集',
-      '三米藍','全家','大四喜牛肉麵','伊卓島','地中海','龍座','黃媽媽','泰麻吉','品味香','洪媽媽',
-      '阿寶','早餐吃啥','麥當勞','肯德基','初八拉麵','歐伊系精緻早餐','豐正食堂','阿基鍋燒麵','窩不知道','我想一下'
-      ,'王仔','嘉農','自己煮'].sample
+     ['不錯', '還好', '不太行'].sample
   end
 
   def chooselunch(received_text)
 
     return nil unless received_text[0..3] == '午餐吃啥'
 
-     ['晨間廚房','武媽媽','81HOME','豪緯麵食館','鍋道一號','外賣','咖哩拌飯','7-11','吃我','不要吃',
-      '三米藍','全家','大四喜牛肉麵','伊卓島','地中海','龍座','黃媽媽','泰麻吉','品味香','洪媽媽',
-      '阿寶','123早餐屋','早餐吃啥','麥當勞','肯德基','初八拉麵','鹿初Brunch','布格早午餐','吉多多早午餐店','歐伊系精緻早餐',
-      '豐正食堂','阿基鍋燒麵','窩不知道','八方雲集','我想一下','王仔','小間早午餐','嘉農'].sample
+     ['晨間廚房','武媽媽','81HOME','地中海','龍座','黃媽媽','泰麻吉','品味香','洪媽媽','阿寶','123'].sample
   end
 
 
@@ -208,11 +185,7 @@ class KamigoController < ApplicationController
 
   # 傳送訊息到 line
   def reply_to_line(reply_text)
-    reply_text = ['蛤','三小','哈哈','喔是喔','what up','用不到','你說的都對','好喔','靠邀','白爛',
-      '好笑嗎','好問題呢','寶','520','7414','幹不要','不知道ㄟ','真的喔','好很喔','好強喔',
-      '你這人真噁心','你很棒','三小','為什麼','什麼拉','再一年','怎麼了','到底是三小','好醜喔','睡起來就好了',
-      '你在搞啥','對阿','真的差不多','真假','怎','喔','好貼心喔','吐了','別搞笑','卯米',
-      '要愛愛','真的ㄟ','0','快樂快樂','感謝你的提醒','發你洗澡卡','(。•́︿•̀。)','勞贖','老子直接在你聊天室大優惠','大大大優惠'].sample if reply_text.nil?
+    return nil if reply_text.nil?
     
     # 取得 reply token
     reply_token = params['events'][0]['replyToken']
@@ -233,6 +206,54 @@ class KamigoController < ApplicationController
        config.channel_secret = '068642867953c0cde3987cb696dccac7'
     config.channel_token = 'VUzbj9NMqRMCyDkbT3STQaXDCpIL7cMhLCTMbkfi153QP3RYghdWgcdFnWs02OHj5UvCZAuW/wsnBgLRwcC/o7dA1Pize8UG8A5Dsr/kIiw1t88GCVFBv8zAQW9jPiqtMIxArSfoXsctpvEN13SpwgdB04t89/1O/w1cDnyilFU='
     }
+  end
+
+
+  def eat
+    render plain: "吃土啦"
+  end 
+
+  def request_headers
+    render plain: request.headers.to_h.reject{ |key, value|
+      key.include? '.'
+    }.map{ |key, value|
+      "#{key}: #{value}"
+    }.sort.join("\n")
+  end
+
+  def response_headers
+    response.headers['5566'] = 'QQ'
+    render plain: response.headers.to_h.map{ |key, value|
+      "#{key}: #{value}"
+    }.sort.join("\n")
+  end
+
+  def request_body
+    render plain: request.body
+  end
+
+  def show_response_body
+    puts "===這是設定前的response.body:#{response.body}==="
+    render plain: "虎哇花哈哈哈"
+    puts "===這是設定後的response.body:#{response.body}==="
+  end
+
+  def sent_request
+    uri = URI('http://localhost:3000/kamigo/eat')
+    http = Net::HTTP.new(uri.host, uri.port)
+    http_request = Net::HTTP::Get.new(uri)
+    http_response = http.request(http_request)
+
+    render plain: JSON.pretty_generate({
+      request_class: request.class,
+      response_class: response.class,
+      http_request_class: http_request.class,
+      http_response_class: http_response.class
+    })
+  end
+
+  def translate_to_korean(message)
+    "#{message}油~"
   end
 
 end
