c524cca4928377285666f51a90cbd349b2465937
siryus <siryus1532@gmail.com>
2021-08-30 00:13:00 +0800

    miller33

--- app/controllers/kamigo_controller.rb
+++ app/controllers/kamigo_controller.rb
@@ -20,9 +20,12 @@ class KamigoController < ApplicationController
 
     # 學說話
     reply_text = learn(channel_id, received_text)
+
+    # 隨機數字
+    reply_text = randnum(received_text) if reply_text.nil?
     
-     # random
-    reply_text = feeling(received_text) if reply_text.nil?
+     # choosedinner
+    reply_text = choosedinner(received_text) if reply_text.nil?
 
     # chooselunch 
     reply_text = chooselunch(received_text) if reply_text.nil?
@@ -139,13 +142,30 @@ class KamigoController < ApplicationController
 
   end
 
-  def feeling(received_text)
+  # 從輸入選項中選一個
 
-    return nil unless received_text[0..6] == '下一場誰會贏'
+  # 隨機數字 69
+  def randnum (received_text)
+
+    return nil unless received_text[0..3] == '隨機數字'
+    
+    received_text = received_text[5..-1]
 
 
+
+    rand(received_text.to_i)
+  end
+
+
+
+  def choosedinner(received_text)
+
+    return nil unless received_text[0..3] == '晚餐吃啥'
       
-     ['PSG','BYG'].sample
+     ['武媽媽','81HOME','豪緯麵食館','鍋道一號','外賣','咖哩拌飯','7-11','吃我','不要吃','八方雲集',
+      '三米藍','全家','大四喜牛肉麵','伊卓島','地中海','龍座','黃媽媽','泰麻吉','品味香','洪媽媽',
+      '阿寶','早餐吃啥','麥當勞','肯德基','初八拉麵','歐伊系精緻早餐','豐正食堂','阿基鍋燒麵','窩不知道','我想一下'
+      ,'王仔','嘉農','自己煮'].sample
   end
 
   def chooselunch(received_text)
@@ -155,7 +175,7 @@ class KamigoController < ApplicationController
      ['晨間廚房','武媽媽','81HOME','豪緯麵食館','鍋道一號','外賣','咖哩拌飯','7-11','吃我','不要吃',
       '三米藍','全家','大四喜牛肉麵','伊卓島','地中海','龍座','黃媽媽','泰麻吉','品味香','洪媽媽',
       '阿寶','123早餐屋','早餐吃啥','麥當勞','肯德基','初八拉麵','鹿初Brunch','布格早午餐','吉多多早午餐店','歐伊系精緻早餐',
-      '豐正食堂','阿基鍋燒麵','窩不知道','八方雲集','我想一下'].sample
+      '豐正食堂','阿基鍋燒麵','窩不知道','八方雲集','我想一下','王仔','小間早午餐','嘉農'].sample
   end
 
 
@@ -213,54 +233,6 @@ class KamigoController < ApplicationController
        config.channel_secret = '068642867953c0cde3987cb696dccac7'
     config.channel_token = 'VUzbj9NMqRMCyDkbT3STQaXDCpIL7cMhLCTMbkfi153QP3RYghdWgcdFnWs02OHj5UvCZAuW/wsnBgLRwcC/o7dA1Pize8UG8A5Dsr/kIiw1t88GCVFBv8zAQW9jPiqtMIxArSfoXsctpvEN13SpwgdB04t89/1O/w1cDnyilFU='
     }
-  end
-
-
-  def eat
-    render plain: "吃土啦"
-  end 
-
-  def request_headers
-    render plain: request.headers.to_h.reject{ |key, value|
-      key.include? '.'
-    }.map{ |key, value|
-      "#{key}: #{value}"
-    }.sort.join("\n")
-  end
-
-  def response_headers
-    response.headers['5566'] = 'QQ'
-    render plain: response.headers.to_h.map{ |key, value|
-      "#{key}: #{value}"
-    }.sort.join("\n")
-  end
-
-  def request_body
-    render plain: request.body
-  end
-
-  def show_response_body
-    puts "===這是設定前的response.body:#{response.body}==="
-    render plain: "虎哇花哈哈哈"
-    puts "===這是設定後的response.body:#{response.body}==="
-  end
-
-  def sent_request
-    uri = URI('http://localhost:3000/kamigo/eat')
-    http = Net::HTTP.new(uri.host, uri.port)
-    http_request = Net::HTTP::Get.new(uri)
-    http_response = http.request(http_request)
-
-    render plain: JSON.pretty_generate({
-      request_class: request.class,
-      response_class: response.class,
-      http_request_class: http_request.class,
-      http_response_class: http_response.class
-    })
-  end
-
-  def translate_to_korean(message)
-    "#{message}油~"
   end
 
 end
