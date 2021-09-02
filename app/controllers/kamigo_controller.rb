997913d30752b70f74186ba822a1c7da00352a8d
siryus <siryus1532@gmail.com>
2021-09-02 21:53:22 +0800

    quiet03

--- app/controllers/kamigo_controller.rb
+++ app/controllers/kamigo_controller.rb
@@ -9,7 +9,7 @@ class KamigoController < ApplicationController
     # 閉嘴
     reply_text = channel_quite(channel_id, received_text)
 
-    reply_text = channel_speak(channel_id, received_text)
+    reply_text = channel_speak(channel_id, received_text) if reply_text.nil?
     
     # 檢測聊天室狀態
     if  channel_status(channel_id).nil?
