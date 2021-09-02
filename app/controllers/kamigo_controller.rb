ed0d8dea1553bf04b5a64dcda740294f14b3bb06
siryus <siryus1532@gmail.com>
2021-09-03 02:20:42 +0800

    quiet03

--- app/controllers/kamigo_controller.rb
+++ app/controllers/kamigo_controller.rb
@@ -5,27 +5,23 @@ class KamigoController < ApplicationController
 
   def webhook
     # 改變聊天室狀態
-    reply_text = change_channel_status(channel_id, received_text)
+
+    reply_text = channel_status(channel_id, received_text)
     
-    # 檢測聊天室狀態
-    channelstatus = channel_status(channel_id) 
-    if channelstatus.nil?
+  
+
+    if ChannelStatus.status == 'quiet'
       # 回應 200
       head :ok
 
       return 
     end
 
-   
 
     # 學說話
     reply_text = learn(channel_id, received_text)
 
-    # 閉嘴
-    reply_text = channel_quite(channel_id, received_text)
-
-
-    reply_text = channel_speak(channel_id, received_text) if reply_text.nil?
+   
 
 
     # 算
@@ -58,20 +54,7 @@ class KamigoController < ApplicationController
   end 
 
 
-   # 改變聊天室狀態關鍵字
-  def change_channel_status(channel_id, received_text)
-    return nil unless received_text[0..5] == '卡米熊安靜' or received_text[0..5] == '卡米熊講話'
-    ChannelStatus.create(channel_id: channel_id, status: 'quiet') if received_text[0..5] == '卡米熊安靜'
-    ChannelStatus.create(channel_id: channel_id, status: 'speak') if received_text[0..5] == '卡米熊講話'
-    received_text
-  end
 
-  # 檢測聊天室狀態
-  def channel_status(channel_id)
-    statuses = ChannelStatus.where(channel_id: channel_id).last&.status
-    return nil if statuses == 'quiet'
-    statuses
-  end
 
 
 
@@ -163,7 +146,31 @@ class KamigoController < ApplicationController
   end
 
 
+
+
+   
+
+
+    # 改變聊天室狀態關鍵字
+  def channel_status(channel_id, received_text)
+    return nil unless received_text[0..4] == '米煮波講話' or  received_text[0..4] == '米煮波安靜' 
+    ChannelStatus.find_or_create_by(channel_id: channel_id; status: 'quiet')
+
+    if  received_text[0..4] == '米煮波講話'
+         ChannelStatus.status = 'speak'
+
+    elsif received_text [0..4] == '米煮波安靜'
+         ChannelStatus.status = 'quiet'
+     end 
+    
+
+    '好'
+  end
+
+
  
+
+
 
 
   def dinner(received_text)
---
+++ app/models/channel_status.rb
@@ -0,0 +1,2 @@
+class ChannelStatus < ApplicationRecord
+end
